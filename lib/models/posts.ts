a57958489c6d1c3981191fdc1ba0143b95d7d423
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 17:54:18 +0200

    fix(models): post types

--- lib/models/posts.ts
+++ lib/models/posts.ts
@@ -1,14 +1,14 @@
 import { Pool } from 'pg'
 
-export interface Post {
+export interface PostBare {
   title: string
   body: string
-  likes: string
-  username: string
 }
 
-export interface PostWithId extends Post {
+export interface Post extends PostBare {
   id: number
+  likes: string
+  username: string
 }
 
 export default function postsModel(db: Pool) {
@@ -17,7 +17,7 @@ export default function postsModel(db: Pool) {
       const sql =
         'SELECT p.id, p.title, p.body, u.username, COUNT(l.postId) as likes FROM Post p LEFT JOIN UserAccount u ON p.userId = u.id LEFT JOIN PostLike l ON p.id = l.postId GROUP BY p.id, u.username'
       const result = await db.query(sql)
-      const posts = result.rows as PostWithId[]
+      const posts = result.rows as Post[]
 
       return posts
     },
@@ -25,14 +25,14 @@ export default function postsModel(db: Pool) {
       const sql =
         'SELECT p.id, p.title, p.body, u.username, COUNT(l.postId) as likes FROM Post p LEFT JOIN UserAccount u ON p.userId = u.id LEFT JOIN PostLike l ON p.id = l.postId  WHERE p.id = $1 GROUP BY p.id, u.username'
       const result = await db.query(sql, [id])
-      const post = result.rows[0] as PostWithId
+      const post = result.rows[0] as Post
       return post
     },
-    createPost: async (userId: number, post: Post) => {
+    createPost: async (userId: number, post: PostBare) => {
       const sql =
         'INSERT INTO Post (title, body, userId) VALUES ($1, $2, $3) RETURNING *'
       const result = await db.query(sql, [post.title, post.body, userId])
-      const createdPost = result.rows[0] as PostWithId
+      const createdPost = result.rows[0] as Post
       return createdPost
     }
   }

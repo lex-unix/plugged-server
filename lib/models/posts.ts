80c6bc13c3144ae1eace4a6f8c1388679cb42fdb
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 18:46:39 +0200

    feat(models): handle like and dislike

--- lib/models/posts.ts
+++ lib/models/posts.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg'
 
-export interface PostBare {
+export interface PostBase {
   title: string
   body: string
 }
 
-export interface Post extends PostBare {
+export interface Post extends PostBase {
   id: number
   likes: string
   username: string
@@ -28,12 +28,28 @@ export default function postsModel(db: Pool) {
       const post = result.rows[0] as Post
       return post
     },
-    createPost: async (userId: number, post: PostBare) => {
+    createPost: async (userId: number, post: PostBase) => {
       const sql =
         'INSERT INTO Post (title, body, userId) VALUES ($1, $2, $3) RETURNING *'
       const result = await db.query(sql, [post.title, post.body, userId])
       const createdPost = result.rows[0] as Post
       return createdPost
+    },
+    likePost: async (postId: string, userId: number) => {
+      let sql = 'SELECT userId from PostLike WHERE postId = $1 AND userId = $2'
+      const liked = await db.query(sql, [postId, userId])
+      if (!liked.rows.length) {
+        sql = 'INSERT INTO PostLike (postId, userId) VALUES ($1, $2)'
+        await db.query(sql, [postId, userId])
+      }
+    },
+    dislikePost: async (postId: string, userId: number) => {
+      let sql = 'SELECT userId from PostLike WHERE postId = $1 AND userId = $2'
+      const liked = await db.query(sql, [postId, userId])
+      if (liked.rows.length) {
+        sql = 'DELETE FROM PostLike WHERE postId = $1 AND userId = $2'
+        await db.query(sql, [postId, userId])
+      }
     }
   }
 }

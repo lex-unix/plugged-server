3d0b2135ddd5d65debf6115eead55c6ff5892a15
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 16:44:30 +0200

    feat(models): add create post

--- lib/models/posts.ts
+++ lib/models/posts.ts
@@ -1,9 +1,12 @@
 import { Pool } from 'pg'
 
 export interface Post {
-  id: number
   title: string
   body: string
+}
+
+export interface PostWithId extends Post {
+  id: number
 }
 
 export default function postsModel(db: Pool) {
@@ -12,7 +15,7 @@ export default function postsModel(db: Pool) {
       const sql =
         'SELECT p.id, p.title, p.body, u.username FROM Post p JOIN UserAccount u ON p.userId = u.id '
       const result = await db.query(sql)
-      const posts = result.rows as Post[]
+      const posts = result.rows as PostWithId[]
 
       return {
         posts
@@ -22,11 +25,18 @@ export default function postsModel(db: Pool) {
       const sql =
         'SELECT p.id, p.title, p.body, u.username FROM Post p JOIN UserAccount u ON p.userId = u.id WHERE p.id = $1'
       const result = await db.query(sql, [id])
-      const post = result.rows[0] as Post
+      const post = result.rows[0] as PostWithId
       console.log(post)
       return {
         post
       }
+    },
+    createPost: async (userId: number, post: Post) => {
+      const sql =
+        'INSERT INTO Post (title, body, userId) VALUES ($1, $2, $3) RETURNING *'
+      const result = await db.query(sql, [post.title, post.body, userId])
+      const createdPost = result.rows[0] as PostWithId
+      return createdPost
     }
   }
 }

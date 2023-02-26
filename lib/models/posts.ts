e297f538caa65a9541635c5d454f83403db0ad21
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 17:20:21 +0200

    fix(models): include total likes in query

--- lib/models/posts.ts
+++ lib/models/posts.ts
@@ -3,6 +3,8 @@ import { Pool } from 'pg'
 export interface Post {
   title: string
   body: string
+  likes: string
+  username: string
 }
 
 export interface PostWithId extends Post {
@@ -13,23 +15,18 @@ export default function postsModel(db: Pool) {
   return {
     getPosts: async () => {
       const sql =
-        'SELECT p.id, p.title, p.body, u.username FROM Post p JOIN UserAccount u ON p.userId = u.id '
+        'SELECT p.id, p.title, p.body, u.username, COUNT(l.postId) as likes FROM Post p LEFT JOIN UserAccount u ON p.userId = u.id LEFT JOIN PostLike l on p.id = l.postId GROUP BY p.id, u.username'
       const result = await db.query(sql)
       const posts = result.rows as PostWithId[]
 
-      return {
-        posts
-      }
+      return posts
     },
     getPost: async (id: string) => {
       const sql =
         'SELECT p.id, p.title, p.body, u.username FROM Post p JOIN UserAccount u ON p.userId = u.id WHERE p.id = $1'
       const result = await db.query(sql, [id])
       const post = result.rows[0] as PostWithId
-      console.log(post)
-      return {
-        post
-      }
+      return post
     },
     createPost: async (userId: number, post: Post) => {
       const sql =

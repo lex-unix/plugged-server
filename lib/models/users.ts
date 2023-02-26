6d6fe170fad933edabd5342cd96da40de962bf3b
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 14:07:56 +0200

    feat(models): add user model

---
+++ lib/models/users.ts
@@ -0,0 +1,31 @@
+import type { Pool } from 'pg'
+
+interface User {
+  id: number
+  username: string
+  password: string
+}
+
+export default function usersModel(db: Pool) {
+  return {
+    registerUser: async (username: string, password: string) => {
+      const sql =
+        'INSERT INTO UserAccount (username, password) VALUES ($1, $2) RETURNING *'
+      const result = await db.query(sql, [username, password])
+      const user = result.rows[0] as User
+
+      return {
+        user
+      }
+    },
+    getUserByUsername: async (username: string) => {
+      const sql =
+        'SELECT id, username, password FROM UserAccount WHERE username = $1'
+      const result = await db.query(sql, [username])
+      const user = result.rows[0] as User
+      return {
+        user
+      }
+    }
+  }
+}

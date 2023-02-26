360d3cb88f39017ef772e69b08b38ba8ab16b9b0
Lex Nousagi <alexmiyin@gmail.com>
2023-02-26 19:00:31 +0200

    fix(routes): update posts schema

--- lib/routes/posts/schema.ts
+++ lib/routes/posts/schema.ts
@@ -1,11 +1,15 @@
 import S from 'fluent-json-schema'
 
+const Profile = S.object()
+  .prop('id', S.number().required())
+  .prop('username', S.string().required())
+
 const Post = S.object()
   .prop('id', S.string().required())
   .prop('title', S.string().required())
   .prop('body', S.string().required())
-  .prop('username', S.string().required())
   .prop('likes', S.string().required())
+  .prop('author', Profile)
 
 const getPosts = {
   response: {
